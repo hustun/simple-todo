ed3d2e641b29603b0f8b33ae6e48333610e93ce2
Hasan Üstün <hasan.ustun@std.yeditepe.edu.tr>
2021-12-07 01:05:22 +0300

    add scroll

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import { Draggable } from "react-beautiful-dnd";
 
 function Task(props) {
     const [classString, setclassString] = useState("");
+    const [done, setDone] = useState(false);
 
     useEffect(() => {
+        console.log("test")
         let colors = ["pink", "purple", "blue", "yellow", "red", "green", "indigo"]
         let color = colors[Math.floor(Math.random() * colors.length)];
-        let classString = "rounded border bg-" + color + "-200 shadow p-4 mb-4"
+        let classString = "rounded border bg-" + color + "-200 shadow p-4 mb-4 flex "
         setclassString(classString);
     }, []);
 
     return (
-        <div className={classString}>
-            <div className="font-bold">{props.title}</div>
-            {props.task}
-        </div>
+        <Draggable key={"task-" + props.index} draggableId={"task-" + props.index} index={props.index}>
+            {(provided) =>
+            (
+                <div ref={provided.innerRef} className={classString + (done ? "line-through text-gray-600" : "")}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+
+                    <Checkbox onClick={() => { setDone(prevDone => !prevDone) }} checked={done} />
+                    <div>
+                        <div className="font-bold">{props.title}</div>
+                        {props.task}
+                        {console.log("teeeest")}
+                    </div>
+
+                </div>
+            )}
+
+        </Draggable>
+
     );
 }
 
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,20 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import Task from "./Task"
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function ToDoList() {
     const [todoList, settodoList] = useState([]);
 
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
-        settodoList([{ title: "Task 1", task: "Read books." }, { title: "Task 2", task: "Do laundry." }])
+        settodoList([{ id: "1", title: "Task 1", task: "Read books." }, { id: "2", title: "Task 2", task: "Do laundry." }])
     }, []);
 
+    // a little function to help us with reordering the result
+    const reorder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    };
+
+    const onDragEnd = (result) => {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const items = reorder(
+            todoList,
+            result.source.index,
+            result.destination.index
+        );
+
+        console.log(items)
+
+        settodoList(items)
+    }
+
+
     return (
-        <div className="w-2/3 max-w-screen-lg mx-auto mt-20">
-            {todoList.map((el, index) => (
-                <Task title={el.title} task={el.task}></Task>
-            ))}
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+                {(provided) => (
+                    <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="w-2/3 max-w-screen-lg mx-auto mt-20">
+                        {todoList.map((el, index) => (
+                            <Task title={el.title} task={el.task} index={index} key={el.id}>{console.log("here")}</Task>
+
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+
+            </Droppable>
+        </DragDropContext >
+
     );
 }
 
