17358f7f90972d1a96b8449c7ce61a38e20124c8
Hasan Üstün <hasan.ustun@std.yeditepe.edu.tr>
2021-12-08 09:58:33 +0300

    design changes

--- src/common/http.js
+++ src/common/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://ec2-18-170-91-239.eu-west-2.compute.amazonaws.com/api",
     headers: {
         "Content-type": "application/json",
     },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
+    options: {
+      safelist: ['bg-green-200', 'bg-pink-200', 'bg-purple-200', 'bg-blue-200', 'bg-yellow-200', 'bg-red-200', 'bg-indigo-200']
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
